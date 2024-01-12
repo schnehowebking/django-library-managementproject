92054dc79f877a68227fe738c36a78268b023dd2
schnehowebking <schnehowebking@gmail.com>
2024-01-12 22:37:47 +0600

    csrf added

--- librarymanagementproject/settings.py
+++ librarymanagementproject/settings.py
@@ -16,8 +16,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# CSRF_TRUSTED_ORIGINS = ['https://islamiyahtechbank.onrender.com', 'http://127.0.0.1:8000']
-# CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
+CSRF_TRUSTED_ORIGINS = ['https://islamiyahtechlibrary.onrender.com', 'http://127.0.0.1:8000']
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
